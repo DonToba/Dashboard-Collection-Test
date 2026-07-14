1f7fc3b8951c4c18f7f02d7de1408ba8169db61b
DonToba <62934609+DonToba@users.noreply.github.com>
2026-07-14 13:08:41 +0100

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,26 +1,29 @@
-from fetch_kobo import fetch_kobo
-from merge import merge_data
-from github_update import upload_geojson
 import os
 from dotenv import load_dotenv
+
+from fetch_kobo import fetch_kobo
+from merge import merge_data
 
 load_dotenv()
 
 BUILDINGS = os.getenv("BUILDING_FILE")
 OUTPUT = os.getenv("OUTPUT_FILE")
 
-print("Fetching Kobo submissions...")
+
+print("Downloading Kobo submissions...")
 kobo = fetch_kobo()
 
+print(f"Retrieved {len(kobo)} records")
+
 print("Joining data...")
+
 merged = merge_data(BUILDINGS, kobo)
+
+print(f"Writing {len(merged)} features...")
 
 merged.to_file(
     OUTPUT,
     driver="GeoJSON"
 )
 
-print("Uploading merged GeoJSON to GitHub...")
-upload_geojson(OUTPUT)
-
-print("Finished successfully.")
+print("Finished.")
